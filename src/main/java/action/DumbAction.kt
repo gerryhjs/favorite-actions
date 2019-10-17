68642bbad94742f1e36c0f71e0684537f0393ec2
Nhan Thai (dandoh) <dainhan605@gmail.com>
2019-10-17 19:20:46 -0400

    this work, wtf?

--- src/main/java/action/DumbAction.kt
+++ src/main/java/action/DumbAction.kt
@@ -2,18 +2,20 @@ package action
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.ui.showOkCancelDialog
-import components.SavedActionsComponent
+import services.SavedActionsService
 
 
 class DumbAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val savedActionsComponent =
-                e.project?.getComponent(SavedActionsComponent::class.java)
-        val currentVal = savedActionsComponent?.savedActionsData?.counter ?: 18
+                e.project?.let {
+                    ServiceManager.getService(it, SavedActionsService::class.java)
+                }
+        val currentVal = savedActionsComponent?.counter ?: 18
         showOkCancelDialog("Message", "Current value is " + currentVal, "Ok")
-        savedActionsComponent?.savedActionsData?.counter = currentVal + 1
+        savedActionsComponent?.counter = currentVal + 1
     }
 }
 
