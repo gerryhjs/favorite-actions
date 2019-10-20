f9dea41d28245e42afe4ce5d378d343bfa28a78f
Nhan Thai (dandoh) <dainhan605@gmail.com>
2019-10-20 00:18:23 -0400

    updates.

--- src/main/java/org/dandoh/favacts/ui/ActionItemForm.java
+++ src/main/java/org/dandoh/favacts/ui/ActionItemForm.java
@@ -1,4 +1,13 @@
 package org.dandoh.favacts.ui;
 
+import com.intellij.ui.SimpleColoredComponent;
+import com.intellij.ui.components.JBLabel;
+
+import javax.swing.*;
+
 public class ActionItemForm {
+    public JPanel content;
+    public SimpleColoredComponent actionName;
+    public SimpleColoredComponent shortcut;
+    public JBLabel actionIcon;
 }
--- src/main/kotlin/org/dandoh/favacts/toolwindows/tool-windows.kt
+++ src/main/kotlin/org/dandoh/favacts/toolwindows/tool-windows.kt
@@ -7,29 +7,24 @@ import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.openapi.wm.ex.ToolWindowEx
 import com.intellij.ui.CollectionListModel
-import com.intellij.ui.SimpleColoredComponent
 import com.intellij.ui.SimpleTextAttributes
 import com.intellij.ui.SimpleTextAttributes.STYLE_PLAIN
-import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.JBList
 import com.intellij.ui.content.ContentFactory
-import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import org.dandoh.favacts.services.ActionId
 import org.dandoh.favacts.services.FavoriteActionsService
+import org.dandoh.favacts.ui.ActionItemForm
 import org.dandoh.favacts.ui.FavoriteActionsToolWindowForm
-import org.dandoh.favacts.utils.cutName
 import org.dandoh.favacts.utils.updateUI
-import java.awt.BorderLayout
 import java.awt.Component
+import javax.swing.JLabel
 import javax.swing.JList
-import javax.swing.JPanel
 import javax.swing.ListCellRenderer
+import javax.swing.SwingConstants
 
 
 class ActionRenderer : ListCellRenderer<ActionId> {
@@ -37,26 +32,25 @@ class ActionRenderer : ListCellRenderer<ActionId> {
                                             actionId: ActionId, index: Int,
                                             isSelected: Boolean, cellHasFocus: Boolean): Component {
     val action = ActionManager.getInstance().getAction(actionId)
-    val panel = JPanel(BorderLayout())
+    val ui = ActionItemForm()
     val bg = UIUtil.getListBackground(isSelected, cellHasFocus)
-    val nameComponent = SimpleColoredComponent()
-    panel.add(nameComponent, BorderLayout.CENTER)
-    panel.add(JBLabel(action.templatePresentation.icon), BorderLayout.WEST)
-    panel.border = JBUI.Borders.empty(2)
-    panel.isOpaque = true
-    panel.background = bg
+
+    ui.content.isOpaque = true
+    ui.content.background = bg
+    ui.actionIcon.icon = action.templatePresentation.icon
     val shortcuts = KeymapUtil.getActiveKeymapShortcuts(actionId)
     val shortcutText = KeymapUtil.getPreferredShortcutText(shortcuts.shortcuts)
-    val str = cutName(action.templateText ?: "", shortcutText, list, panel, nameComponent)
-    nameComponent.background = bg
-    nameComponent.append(str, SimpleTextAttributes(STYLE_PLAIN,
+    val str = action.templateText ?: ""
+    ui.actionName.background = bg
+    ui.actionName.append(str, SimpleTextAttributes(STYLE_PLAIN,
         defaultActionForeground(isSelected, cellHasFocus, null)))
     val groupFg = if (isSelected) UIUtil.getListSelectionForeground(true) else UIUtil.getInactiveTextColor()
     if (shortcutText.isNotEmpty()) {
-      nameComponent.append(" $shortcutText",
-          SimpleTextAttributes(SimpleTextAttributes.STYLE_SMALLER or SimpleTextAttributes.STYLE_BOLD, groupFg))
+      val style = SimpleTextAttributes.STYLE_SMALLER or
+          SimpleTextAttributes.STYLE_BOLD
+      ui.shortcut.append(" $shortcutText", SimpleTextAttributes(style, groupFg))
     }
-    return panel
+    return ui.content
   }
 
 }
@@ -68,7 +62,7 @@ class FavoriteActionsToolWindow(project: Project) : FavoriteActionsService.Liste
   init {
     val favoriteActionsService =
         ServiceManager.getService(project, FavoriteActionsService::class.java)
-    with (ui.actionList) {
+    with(ui.actionList) {
       cellRenderer = ActionRenderer()
       model = CollectionListModel(favoriteActionsService.getActionIds())
     }
